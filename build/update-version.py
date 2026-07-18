a2ba90c646004db4cb2863eb4f09bfe1a97bf755
jasperhabicht <mail@jasperhabicht.de>
2026-07-18 23:42:57 +0200

    Update versioning script

--- build/update-version.py
+++ build/update-version.py
@@ -2,12 +2,16 @@ import sys
 import json
 import pathlib
 import re
+from datetime import datetime
 
 if len(sys.argv) != 3:
     sys.exit('Usage: update-version.py <version> <date>.')
 
 update_version = sys.argv[1]
 update_date = sys.argv[2]
+
+date_data = datetime.strptime(update_date, '%Y-%m-%d')
+update_date_format = f'{date_data.day} {date_data.strftime('%B')} {date_data.year}'
 
 pattern_version = re.compile(r'\d+\.\d+\.\d+')
 pattern_date = re.compile(r'\d{4}-\d{2}-\d{2}')
@@ -32,13 +36,22 @@ for replacement in update_replacements:
             count=1,
             flags=re.MULTILINE
         )
-        file_text = re.sub(
-            replacement['pattern_date'],
-            replacement['replace_date'].format(date=update_date),
-            file_text,
-            count=1,
-            flags=re.MULTILINE
-        )
+        if replacement['date_format']:
+            file_text = re.sub(
+                replacement['pattern_date'],
+                replacement['replace_date'].format(date=update_date_format),
+                file_text,
+                count=1,
+                flags=re.MULTILINE
+            )
+        else:
+            file_text = re.sub(
+                replacement['pattern_date'],
+                replacement['replace_date'].format(date=update_date),
+                file_text,
+                count=1,
+                flags=re.MULTILINE
+            )
 
         update_file.write_text(file_text, newline='\n')
 
