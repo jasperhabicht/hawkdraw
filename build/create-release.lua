4acde9af7315e3e2fd8fa5d5780e4aa1cb3ac764
jasperhabicht <mail@jasperhabicht.de>
2026-07-20 23:32:55 +0200

    Update create-version.lua

--- build/create-release.lua
+++ build/create-release.lua
@@ -8,4 +8,14 @@
 
 os.execute('cd .. && git archive --prefix=hawkdraw/ --output=hawkdraw.zip HEAD')
 
+local release_file = io.open('../hawkdraw-doc.tex', 'rb')
+local release_data = release_file:read('*a')
+release_file:close()
+
+local release_version = string.match(release_data, '\\def\\hawkdrawfileversion{(%d+%.%d+%.%d+)}')
+
+local release_changes = string.match(release_data, '\\changes{v' .. release_version .. '}{%d%d%d%d/%d%d/%d%d}{([^}]*)}')
+print('Version ' .. release_version .. ':')
+print(release_changes)
+
 -- EOF
